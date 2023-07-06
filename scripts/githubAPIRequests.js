6f9ec04b14cd99f7fe8136263c76f80a1f2665dd
Adil Alizada <aa2130@hw.ac.uk>
2023-07-07 01:23:00 +0400

    Finished a function that retrieves the contents of the 'readme' file

--- scripts/githubAPIRequests.js
+++ scripts/githubAPIRequests.js
@@ -1,3 +1,18 @@
+/** Function will send an API request to GitHub and expect all the files from a repository
+*/
+export async function getGitReadme(url) {
+   let readme;
+
+   try {
+       readme = await fetch(url);
+   } catch (error) {
+       readme = "Sorry, cannot connect to GitHub";
+       console.error(error);
+   }
+
+   return readme.text();
+}
+
 export function getRepo(owner, repoName, url, headers){
     /* This is for getting the basic details of the repo */
     fetch(url, { headers })
--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,25 @@
 import { getCurrentURL } from './getCurrentURL.js';
-import { getRepo, searchFileInDirectory, getFileContent }from './githubAPIRequests.js';
+import { getRepo, searchFileInDirectory, getFileContent, getGitReadme } from './githubAPIRequests.js';
 
 let currentUrlString = await getCurrentURL();
 let urlSplit = currentUrlString.split("/");
 
+let summary = ""
+
 let owner = urlSplit[3];
 let repoName = urlSplit[4];
 
-const URL = `https://api.github.com/repos/${owner}/${repoName}`;
+const URL = `https://raw.githubusercontent.com/${owner}/${repoName}`;
+const READMEURL = URL + '/main/README.md'
 const CONTENTSURL = URL + `/contents`
 
 const HEADERS = {
     'Accept': 'application/vnd.github.v3+json',
 };
 
-/** Function will send an API request to GitHub and expect all the files from a repository
- * 
- * @param {string} repoURL --> The url that has to be used for an API request 
- */
-function getGitSummary(repoURL) {
-}
 
 if (urlSplit.length == 5 && urlSplit[2] == "github.com") {
-    document.getElementById("summary-container").innerHTML = currentUrlString
-    getGitSummary(currentUrlString);
+	document.getElementById("summary-container").innerHTML = await getGitReadme(READMEURL); // Just a test
+} else {
+	summary = "Sorry, but this isn't a valid GitHub repository."
 }
-  
-document.getElementById("summary-container").innerHTML = CONTENTSURL; // Just a test
-
-// Search for the target file in the repository
-// searchFileInDirectory(CONTENTSURL)
-//     .catch(error => {
-//         console.error('Error:', error);
-//     }
-// );
-
-// use this to retrieve file content   
-// getFileContent(filePath)
-// 	.then(content => {
-// 		console.log('File Content:', content);
-// 	})
-// 	.catch(error => {
-// 		console.error('Error:', error);
-// 	});
